8292e6deb6bf482cd2f5b55b5291b39200b9aa4b
George Li <georgejdli@gmail.com>
2014-11-20 00:01:13 -0800

    Getting started. Enemies instantiaed with random starting positions.

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
 // Enemies our player must avoid
 var Enemy = function() {
+    //give each enemy a random starting position
+    this.x = getRandomInt(1, 5) * 101;
+    this.y = getRandomInt(2, 5) * 83;
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-}
+};
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -14,23 +16,44 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-}
+
+};
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.sprite = 'images/char-boy.png';
 
 
+};
+Player.prototype.update = function(dt) {
+
+};
+Player.prototype.render = function() {
+
+};
+Player.prototype.handleInput = function(keyPress) {
+    if (keyPress === 'left') {
+
+    }
+};
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies = [];
+(function () {
+    for (var i = 0; i < 6; i++) {
+        allEnemies[i] = new Enemy();
+    }
+})();
 
-
+var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
---
+++ js/helper.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Hellfire on 11/19/14.
+ */
+// Returns a random number between min (inclusive) and max (exclusive)
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
